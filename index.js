8df873ad0126a18ad63f096e1e472b2c589e5625
Kıvanç Çoban <kivanc0556@gmail.com>
2024-08-18 11:01:59 +0000

    file data microservice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,12 +8,21 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var upload = multer({ dest: 'uploads/' });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
